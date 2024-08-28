cb1164fb837ec781c13271c09f3df8f49b2b6a7b
noguchi5tag <noguchi@5tag.co.jp>
2024-08-28 15:54:20 +0900

    画像素材

--- database/factories/InertiaJobFactory.php
+++ database/factories/InertiaJobFactory.php
@@ -9,37 +9,36 @@ class InertiaJobFactory extends Factory
 {
     protected $model = InertiaJob::class;
 
-    public function definition()
+    public function definition(): array
     {
-        $faker = $this->faker;
-        $holidays = ['土曜日', '日曜日', '祝祭日'];
-
-        $workDays = [];
-        while (count($workDays) < 5) {
-            $day = $faker->dayOfWeek;
-            if (in_array($day, ['月曜日', '火曜日', '水曜日', '木曜日', '金曜日'])) {
-                $workDays[] = $day;
-            }
+        $companyNames = [];
+        for ($i = 1; $i <= 10; $i++) {
+            $companyNames[] = "株式会社テスト" . str_pad($i, 2, '0', STR_PAD_LEFT);
         }
-
+    
         return [
-            'companyName' => $faker->company,
-            'WantedTitles' => $faker->jobTitle,
-            'Occupation' => $faker->jobTitle,
-            'companyAddress' => $faker->address,
-            'companyPay' => $faker->numberBetween(300000, 1000000),
-            'dutyStation' => $faker->city,
-            'workDescription' => $faker->text,
-            'payDescription' => $faker->text,
-            'travelExpenses' => $faker->numberBetween(1000, 5000),
-            'Welfare' => $faker->text,
-            'startWork' => $faker->time,
-            'endWork' => $faker->time,
-            'workDays' => implode(', ', $workDays), // 月曜日～金曜日
-            'freeDays' => $faker->randomElement($holidays), // 土曜日・日曜日・祝祭日
-            'NearestStation' => $faker->streetName,
-            'workOther' => $faker->text,
-            'status' => $faker->randomElement([0, 1]), // 0: 非公開, 1: 公開
+            'companyName' => $this->faker->unique()->randomElement($companyNames),
+            'WantedTitles' => $this->faker->randomElement(['営業マネージャー', '事務スタッフ', 'システムエンジニア', 'デザイナー', 'カスタマーサポート']),
+            'Occupation' => $this->faker->randomElement(['営業', '事務', 'エンジニア', 'デザイナー', 'その他']),
+            'companyAddress' => $this->faker->address,
+            'companyPay' => $this->faker->numberBetween(150000, 500000),
+            'dutyStation' => $this->faker->randomElement(['長崎市', '島原市', '南島原市', '諫早市', '雲仙市', '西海市', '五島市', '新上五島市', '松浦市', '壱岐市', '対馬市', '平戸市']),
+            'workDescription' => $this->faker->text(200),
+            'payDescription' => $this->faker->randomElement(['月給制', '年棒', '日払い']),
+            'travelExpenses' => $this->faker->numberBetween(3000, 20000),
+            'Welfare' => $this->faker->text(50),
+            'startWork' => $this->faker->numberBetween(8, 10),
+            'endWork' => $this->faker->numberBetween(17, 19),
+            'workDays' => $this->faker->randomElement(['月～金', 'シフト制']),
+            'freeDays' => $this->faker->randomElement(['土日', '週休2日制']),
+            'NearestStation' => $this->faker->randomElement(['中央駅', '南駅', '東駅', '西駅', '北駅']),
+            'workOther' => $this->faker->text(100),
+            'status' => $this->faker->numberBetween(0, 1),
+            'image1' => null,
+            'image2' => null,
+            'image3' => null,
+            'image4' => null,
+            'image5' => null,
         ];
     }
 }
